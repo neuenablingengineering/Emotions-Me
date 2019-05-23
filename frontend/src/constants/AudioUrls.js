e06addd6c4871c7c61f2b7388c7cde5385040831
enableadmin <38411835+enableadmin@users.noreply.github.com>
2019-05-23 17:52:48 -0400

    Changes for AWS

--- frontend/src/constants/AudioUrls.js
+++ frontend/src/constants/AudioUrls.js
@@ -1,263 +1,263 @@
 const angryAudio = [
     {
         label: 'Extremely Angry - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1014.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IEO_ANG_EX.wav'
     },
     {
         label: 'Very Angry - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1012.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/TAI_ANG_VE.wav'
     },
     {
         label: 'Very Angry - "I think I\'ve seen this before"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/100.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/ITS_ANG_VE.wav'
     },
     {
         label: 'Very Angry - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1000.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IEO_ANG_VE.wav'
     },
     {
         label: 'Very Angry - "We\'ll stop in a couple of minutes"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/104.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/WSI_ANG_VE.wav'
     },
     {
         label: 'Very Angry - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1009.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/DFA_ANG_VE.wav'
     },
     {
         label: 'Somewhat Angry - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/10.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/TAI_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "I wonder what this is about"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1005.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IWW_ANG_SO.wav'
     },
     {
-        label: 'Somewhat Angry - "I think I\'vs seen this before"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1006.wav'
+        label: 'Somewhat Angry - "I think I\'ve seen this before"',
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/ITS_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "We\'ll stop in a couple of minutes"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1007.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/WSI_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "The surface is slick"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1008.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/TSI_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1011.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IEO_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "I think I have a doctor\'s appointment"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1015.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/ITH_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "I\'m on my way to the meeting"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1016.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IOM_ANG_SO.wav'
     },
     {
         label: 'Somewhat Angry - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1018.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/DFA_ANG_SO.wav'
     },
     {
         label: 'A Little Angry - "We\'ll stop in a couple of minutes"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1013.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/WSI_ANG_LI.wav'
     },
     {
         label: 'A Little Angry - "I wonder what this is about"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1017.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/IWW_ANG_LI.wav'
     },
     {
         label: 'A Little Angry - "I think I have a doctor\'s appointment"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/ANGRY/1010.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Angry/ITH_ANG_LI.wav'
     }
 ];
 
 const disgustAudio = [
     {
         label: 'Very Disgusted - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/0.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IEO_DIS_VE.wav'
     },
     {
         label: 'Very Disgusted - "I would like a new alarm clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1018.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IWL_DIS_VE.wav'
     },
     {
         label: 'Very Disgusted - "The surface is slick"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1012.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/TSI_DIS_VE.wav'
     },
     {
         label: 'Very Disgusted - "I\'m on my way to the meeting"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1009.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IOM_DIS_VE.wav'
     },
     {
         label: 'Very Disgusted - "I think I have a doctor\'s appointment"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1010.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/ITH_DIS_VE.wav'
     },
     {
         label: 'Very Disgusted - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1005.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/TAI_DIS_VE.wav'
     },
     {
         label: 'Somewhat Disgusted - "It\'s 11 o\'clock" 1',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/10.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IEO_DIS_SO1.wav'
     },
     {
         label: 'Somewhat Disgusted - "I wonder what this is about"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1000.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IWW_DIS_SO.wav'
     },
     {
         label: 'Somewhat Disgusted - "We\'ll stop in a couple of minutes"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/104.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/WSI_DIS_SO.wav'
     },
     {
         label: 'Somewhat Disgusted - "It\'s 11 o\'clock" 2',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1008.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IEO_DIS_SO2.wav'
     },
     {
         label: 'Somewhat Disgusted - "I\'m on my way to the meeting"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1011.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IOM_DIS_SO.wav'
     },
     {
         label: 'Somewhat Disgusted - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1014.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/TAI_DIS_SO.wav'
     },
     {
         label: 'A Little Disgusted - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1006.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/IEO_DIS_LI.wav'
     },
     {
         label: 'A Little Disgusted - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1007.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/DFA_DIS_LI.wav'
     },
     {
         label: 'A Little Disgusted - "That is exactly what happened"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1016.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/TIE_DIS_LI.wav'
     },
     {
         label: 'A Little Disgusted - "I think I\'ve seen this before"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/DISGUST/1017.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Disgust/ITS_DIS_LI.wav'
     },
 ];
 
 const fearAudio = [
     {
         label: 'Very Fearful - "That is exactly what happened"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1016.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/TIE_FEA_VE.wav'
     },
     {
         label: 'Very Fearful - "I wonder what this is about"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1017.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IWW_FEA_VE.wav'
     },
     {
         label: 'Very Fearful - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/104.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/DFA_FEA_VE.wav'
     },
     {
         label: 'Very Fearful - "I would like a new alarm clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1014.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IWL_FEA_VE.wav'
     },
     {
         label: 'Very Fearful - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1009.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IEO_FEA_VE.wav'
     },
     {
         label: 'Somewhat Fearful - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/100.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/TAI_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "I think I\'ve seen this before"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/10.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/ITS_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "I wonder what this is about"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1007.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IWW_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "Maybe tomorrow it will be cold"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1010.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/MTI_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1013.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IEO_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "That is exactly what happened"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1015.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/TIE_FEA_SO.wav'
     },
     {
         label: 'Somewhat Fearful - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1011.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/DFA_FEA_SO.wav'
     },
     {
         label: 'A Little Fearful - "I think I have a doctor\'s appointment"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1008.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/ITH_FEA_LI.wav'
     },
     {
         label: 'A Little Fearful - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1012.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/DFA_FEA_LI.wav'
     },
     {
         label: 'A Little Fearful - "Maybe tomorrow it will be cold"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1000.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/MTI_FEA_LI.wav'
     },
     {
         label: 'A Little Fearful - "That is exactly what happened"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1005.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/TIE_FEA_LI.wav'
     },
     {
         label: 'A Little Fearful - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/1006.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Fear/IEO_FEA_LI.wav'
     },
 ];
 
 const happyAudio = [
     {
         label: 'Very Happy - "I would like a new alarm clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/4.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/IWL_HAP_VE.wav'
     },
     {
         label: 'Somewhat Happy - "I\'m on my way to the meeting"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/2.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/IOM_HAP_SO.wav'
     },
     {
         label: 'Somewhat Happy - "The airplane is almost full"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/7.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/TAI_HAP_SO.wav'
     },
     {
         label: 'Somewhat Happy - "That is exactly what happened"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/5.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/TIE_HAP_SO.wav'
     },
     {
         label: 'Somewhat Happy - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/6.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/DFA_HAP_SO.wav'
     },
     {
         label: 'A Little Happy - "Don\'t forget a jacket"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Happy/DFA_HAP_LI.wav'
     },
 ];
 
 const neutralAudio = [
     {
         label: 'Neutral - "It\'s 11 o\'clock"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Neutral/IEO_NEU.wav'
     },
     {
         label: 'Neutral - "We\'ll stop in a couple of minutes"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/2.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Neutral/WSI_NEU.wav'
     },
     {
         label: 'Neutral - "The surface is slick"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/3.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Neutral/TSI_NEU.wav'
     },
     {
         label: 'Neutral - "I think I\'ve seen this before"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/4.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Neutral/ITS_NEU.wav'
     },
     {
         label: 'Neutral - "Maybe tomorrow it will be cold"',
-        url: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/6.wav'
+        url: 'https://s3.amazonaws.com/emotions-and-me-bucket/Audio/Neutral/MTI_NEU.wav'
     },
 ];
 
